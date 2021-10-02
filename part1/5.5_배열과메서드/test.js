487d64557ffeb1bc5b786df078244ac56bfa4ed8
qwe5507 <apvmffkdls@naver.com>
2021-10-02 10:55:16 +0900

    5.5 배열과 메서드 과제 풀이

--- part1/5.5_배열과메서드/test.js
+++ part1/5.5_배열과메서드/test.js
@@ -54,3 +54,64 @@ describe("filterRangeInPlace", function () {
     });
 
 });
+describe("Calculator", function () {
+    let calculator;
+
+    before(function () {
+        calculator = new Calculator;
+    });
+
+    it("calculate(12 + 34) = 46", function () {
+        assert.equal(calculator.calculate("12 + 34"), 46);
+    });
+
+    it("calculate(34 - 12) = 22", function () {
+        assert.equal(calculator.calculate("34 - 12"), 22);
+    });
+
+    it("add multiplication: calculate(2 * 3) = 6", function () {
+        calculator.addMethod("*", (a, b) => a * b);
+        assert.equal(calculator.calculate("2 * 3"), 6);
+    });
+
+    it("add power: calculate(2 ** 3) = 8", function () {
+        calculator.addMethod("**", (a, b) => a ** b);
+        assert.equal(calculator.calculate("2 ** 3"), 8);
+    });
+});
+describe("unique", function () {
+    it("removes non-unique elements", function () {
+        let strings = ["Hare", "Krishna", "Hare", "Krishna",
+            "Krishna", "Krishna", "Hare", "Hare", ":-O"
+        ];
+
+        assert.deepEqual(unique(strings), ["Hare", "Krishna", ":-O"]);
+    });
+
+    it("does not change the source array", function () {
+        let strings = ["Krishna", "Krishna", "Hare", "Hare"];
+        unique(strings);
+        assert.deepEqual(strings, ["Krishna", "Krishna", "Hare", "Hare"]);
+    });
+});
+describe("groupById", function () {
+
+    it("creates an object grouped by id", function () {
+        let users = [
+            { id: 'john', name: "John Smith", age: 20 },
+            { id: 'ann', name: "Ann Smith", age: 24 },
+            { id: 'pete', name: "Pete Peterson", age: 31 },
+        ];
+
+        assert.deepEqual(groupById(users), {
+            john: { id: 'john', name: "John Smith", age: 20 },
+            ann: { id: 'ann', name: "Ann Smith", age: 24 },
+            pete: { id: 'pete', name: "Pete Peterson", age: 31 },
+        });
+    });
+
+    it("works with an empty array", function () {
+        users = [];
+        assert.deepEqual(groupById(users), {});
+    });
+});
