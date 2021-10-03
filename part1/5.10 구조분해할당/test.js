1f6609f12d27988e6f194f013888cfcbe15a9e8e
qwe5507 <apvmffkdls@naver.com>
2021-10-03 18:09:33 +0900

    5.9 구조 분해 할당

--- part1/5.10 구조분해할당/test.js
+++ part1/5.10 구조분해할당/test.js
@@ -1,29 +1,15 @@
-describe("sumSalaries", function() {
-  it("returns sum of salaries", function() {
+describe("topSalary", function() {
+  it("returns top-paid person", function() {
     let salaries = {
       "John": 100,
       "Pete": 300,
       "Mary": 250
     };
 
-    assert.equal( sumSalaries(salaries), 650 );
+    assert.equal( topSalary(salaries), "Pete" );
   });
 
-  it("returns 0 for the empty object", function() {
-    assert.strictEqual( sumSalaries({}), 0);
-  });
-});
-
-describe("count", function() {
-  it("counts the number of properties", function() {
-    assert.equal( count({a: 1, b: 2}), 2 );
-  });
-
-  it("returns 0 for an empty object", function() {
-    assert.equal( count({}), 0 );
-  });
-
-  it("ignores symbolic properties", function() {
-    assert.equal( count({ [Symbol('id')]: 1 }), 0 );
+  it("returns null for the empty object", function() {
+    assert.isNull( topSalary({}) );
   });
 });
