3444197600428ffc45548cf6320ae643a36e9e70
qwe5507 <apvmffkdls@naver.com>
2021-11-07 17:09:28 +0900

    13.3 모듈 동적 로딩

---
+++ part1/13.3 모듈 동적 로딩/say.js
@@ -0,0 +1,12 @@
+// 📁 say.js
+export function hi() {
+  alert(`안녕하세요.`);
+}
+
+export function bye() {
+  alert(`안녕히 가세요.`);
+}
+
+export default function() {
+  alert("export default한 모듈을 불러왔습니다!");
+}
